7242d14638962d118454bb6b23a51ad0da68f563
Guilherme Carvalho <guicarv99@gmail.com>
2023-12-11 20:45:10 +0000

    done

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-const jsonServer = require("json-server");
-const morgan = require("morgan");
+require('dotenv').config();
+const jsonServer = require('json-server');
+const morgan = require('morgan');
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT;
 
 server.use(middlewares);
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
   // Middleware to disable CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 server.use(router);
@@ -19,3 +19,6 @@ server.use(router);
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
 });
+
+//* http://localhost:5005/projects/1?_embed=tasks
+//* 1?_embed=tasks -> this will include the tasks associated with the specific project
